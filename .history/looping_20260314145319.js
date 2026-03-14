f692c1519cc8234fb5762f096aa337cb645f9dcd
Tanim <tanimtanzilislam9@gmail.com>
2026-03-14 15:00:44 +0600

    module 16.5.1 Array traversal using for & while loop

---
+++ .history/looping_20260314145319.js
@@ -0,0 +1,32 @@
+// looping Techniques
+
+//  1.for loop
+//2.while loop
+//3.do while-->ignore
+//4. for of -->array loop
+//5.for in-->object loop korar jonno
+//
+
+const friends=['Elon','Bill','Mark','Waren']
+
+for(const friend of friends){
+    console.log(friend);
+}
+
+for(let i=0;i<10;i++){
+    console.log(i);
+    console.log(friends[i]);
+}
+
+for(let i=0;i<friends.length;i++){
+    console.log(i);
+    console.log(friends[i]);
+
+
+    
+}
+
+
+
+const numbers = [5,23,456,421,25,4512,54541,21,45,6]
+
