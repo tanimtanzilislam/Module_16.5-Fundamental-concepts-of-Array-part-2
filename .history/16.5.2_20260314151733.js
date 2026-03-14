abcd3bf67861f957f97d713674bb5119a3bcb63f
Tanim <tanimtanzilislam9@gmail.com>
2026-03-14 16:23:07 +0600

    16.5_2 Reversing an array with/without reverse method

---
+++ .history/16.5.2_20260314151733.js
@@ -0,0 +1,13 @@
+
+const numbers=[1,2,3,4,5,6,7]
+
+
+// numbers.reverse();
+// console.log(numbers)
+
+
+// const reversed =numbers.reverse();
+// console.log(reversed);
+
+
+const rev_numbers=[]
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+// numbers.reverse();
+// // console.log(numbers)
+
+
+// // const reversed =numbers.reverse();
+// // console.log(reversed);
+
+
+// const rev_numbers=[];
+
+// for(const num of numbers){
+//     console.log(num);
+//     // evabe push korle pison theke push hoi, tay same result ashe
+
+//     rev_numbers.push(numbers);
+
+//     // unshift diye push korle samne diye push hoi, reverse result ashe
+
+//     rev_numbers.unshift(num)
+// }
+
+// console.log(rev_numbers)
